9ca1b895d96f910d76b50ae71e077785a0e45c99
Roland Lehmann <rlehmann@pixeltricks.de>
2011-07-07 09:16:50 +0200

    adjusted the license comment

--- lang/en_GB.php
+++ lang/en_GB.php
@@ -2,20 +2,20 @@
 /**
  * Copyright 2010, 2011 pixeltricks GmbH
  *
- * This file is part of SilvercartPrepaymentPayment.
+ * This file is part of SilvercartPaymentIPayment.
  *
  * SilvercartPaymentIPayment is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SilvercartPrepaymentPayment is distributed in the hope that it will be useful,
+ * SilvercartPaymentIPayment is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with SilvercartPrepaymentPayment.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SilvercartPaymentIPayment.  If not, see <http://www.gnu.org/licenses/>.
  *
  * English (GB) language pack
  *
